66e4929271b8314fed50b3c0c58f6e78cdb96160
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-12 16:03:02 +0100

    feat: create product controller

---
+++ src/main/java/com/bentoo/spring_mvc/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.bentoo.spring_mvc.controller;
+
+import com.bentoo.spring_mvc.model.Product;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public  class ProductController {
+    private List<Product> products = Arrays.asList(
+            new Product("Camiseta", 49.90),
+            new Product("Calça Jeans", 99.90),
+            new Product("Tênis", 199.90)
+    );
+    @GetMapping
+    public String GetProducts(Model model){
+
+        model.addAttribute("products",products);
+
+        return "products dsss";
+    }
+}
--- src/main/java/com/bentoo/spring_mvc/model/Product.java
+++ src/main/java/com/bentoo/spring_mvc/model/Product.java
@@ -12,6 +12,7 @@ public class Product {
     }
 
     public Product(String name, double price){
+        this.id = UUID.randomUUID();
         this.name = name;
         this.price = price;
     }
