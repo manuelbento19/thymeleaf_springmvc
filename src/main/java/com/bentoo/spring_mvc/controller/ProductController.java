9a49bd0b649221df760c30f5ca7cc9706b4b06cf
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-12 17:20:47 +0100

    feat: product list view

--- src/main/java/com/bentoo/spring_mvc/controller/ProductController.java
+++ src/main/java/com/bentoo/spring_mvc/controller/ProductController.java
@@ -1,14 +1,17 @@
 package com.bentoo.spring_mvc.controller;
 
 import com.bentoo.spring_mvc.model.Product;
+import jakarta.websocket.server.PathParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping
@@ -23,5 +26,15 @@ public  class ProductController {
 
         model.addAttribute("products",products);
         return "index";
+    }
+    @GetMapping("/{id}")
+    public String GetProductDetails(@PathVariable UUID id, Model model){
+        var productExists = products.stream().filter(item->item.id.equals(id)).findFirst();
+        if(!productExists.isEmpty()){
+            model.addAttribute("product",productExists.get());
+            System.out.println(productExists.get());
+            return "details";
+        }
+        return "Product doesn't exists";
     }
 }
